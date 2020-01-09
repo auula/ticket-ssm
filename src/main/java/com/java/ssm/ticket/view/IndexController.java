7f5a2b34bf64022b761c3902ddd66e2ef3659370
Deencode <deen.job@qq.com>
2020-01-09 19:11:06 +0800

    add init  ssm  all xml config file

---
+++ src/main/java/com/java/ssm/ticket/view/IndexController.java
@@ -0,0 +1,16 @@
+package com.java.ssm.ticket.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+	@ResponseBody
+	@GetMapping(value = {"/index.html","/"})
+	public String index() {
+		return "ticket";
+	}
+}
